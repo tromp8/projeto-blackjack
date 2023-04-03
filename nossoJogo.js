19d27875f1fed1ed508b98ba09648a8cd5f77b18
Débora Lucia Koppe Soares <deboraluciak@hotmail.com>
2023-04-03 14:10:42 -0300

    Nosso jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,33 @@
  * 
  * 
  */
+
+console.log('Bem-vindo ao Blackjack!')
+
+
+if (confirm('Quer inicar uma nova rodada?')) {  
+   const carta1JOG = comprarCarta();
+   const carta2JOG = comprarCarta();
+   
+   const pontuacaoJOG = carta1JOG.valor + carta2JOG.valor
+   console.log('Usuario: cartas - ' ,carta1JOG.texto, carta2JOG.texto, '-', pontuacaoJOG);
+
+   const carta1CPU = comprarCarta();
+   const carta2CPU = comprarCarta();
+   
+   const pontuacaoCPU = carta1CPU.valor + carta2CPU.valor
+   console.log('CPU: cartas - ' ,carta1CPU.texto, carta2CPU.texto, '-', pontuacaoCPU)
+   
+
+   
+   if (pontuacaoJOG > pontuacaoCPU) {
+      console.log('VOCE GANHOU!')
+   } else if (pontuacaoJOG < pontuacaoCPU) {
+      console.log('VOCE PERDEU!')
+   } else {
+      console.log('EMPATE!')
+   }
+
+} else {
+   console.log('FIM DO JOGO')
+}
